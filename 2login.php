b8f4e4b8d0d35a00f382ce77e480649b08a489b8
oktfiaa <okta77131@gmail.com>
2026-02-26 01:22:08 +0700

    add hal login

---
+++ 2login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Sistem</title>
+</head>
+<body>
+
+<h2>Login Sistem Keuangan Informasi Transaksi & Pengelolaan Laporan Keuangan</h2>
+
+<form method="POST" action="proses_login.php">
+
+    Username:<br>
+    <input type="text" name="username"><br><br>
+
+    Password:<br>
+    <input type="password" name="password"><br><br>
+
+    <button type="submit" name="login">Login</button>
+
+</form>
+
+</body>
+</html>
