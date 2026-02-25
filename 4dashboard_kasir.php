eea06738e0bb1aae04938676f29a0d77405c9a51
oktfiaa <okta77131@gmail.com>
2026-02-26 01:49:00 +0700

    add dash kasir

---
+++ 4dashboard_kasir.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header("Location: 2login.php");
+    exit;
+}
+?>
+
+<h2>Dashboard Kasir</h2>
+<p>Halo <?php echo $_SESSION['nama']; ?></p>
+
+<a href="">Logout</a>
