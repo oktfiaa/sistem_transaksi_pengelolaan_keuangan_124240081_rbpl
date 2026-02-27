7b7f4f81f6a6f08718b4c2f8fa6b2e0b4c2cfae3
oktfiaa <okta77131@gmail.com>
2026-02-27 14:19:56 +0700

    add dash admin & owner

---
+++ dashboard_admin_keuangan.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header("Location: index.php");
+    exit;
+}
+?>
+
+<h2>Dashboard Kasir</h2>
+<p>Halo <?php echo $_SESSION['nama']; ?></p>
+
+<a href="5logout.php">Logout</a>
+<a href="">Buka Dashboard Admin Keuangan</a>
