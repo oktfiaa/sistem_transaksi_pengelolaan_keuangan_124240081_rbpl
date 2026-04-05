1fb579612202abf1dd373baf7691a5c9cc2782d4
oktfiaa <okta77131@gmail.com>
2026-04-05 23:45:21 +0700

    edit transaksi

--- transaksi.php
+++ transaksi.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION['username']) || $_SESSION['role'] != '1'){
 }
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
     <title>Input Transaksi</title>
@@ -36,7 +36,7 @@ if(!isset($_SESSION['username']) || $_SESSION['role'] != '1'){
 
     <button type="submit" name="simpan">Simpan Transaksi</button>
 
-</form>
+</form> -->
 
 <!-- <script>
 function tambahInput(){
@@ -44,6 +44,85 @@ function tambahInput(){
     div.innerHTML += '<br>Nominal: <input type="number" name="nominal[]" required> <button type="button" onclick="this.parentNode.remove()">Hapus</button>';
 }
 </script> -->
-
+<!-- 
 </body>
-</html>
+</html> -->
+
+<?php
+session_start();
+include "1koneksi.php";
+
+//tambah barang
+if(isset($_POST['tambah'])){
+    $kode = $_POST['kode_barang'];
+    $qty  = $_POST['qty'];
+
+    $query = mysqli_query($connection, 
+        "SELECT * FROM barang WHERE kode_barang='$kode'"
+    );
+
+    $barang = mysqli_fetch_assoc($query);
+
+    if($barang){
+        if(!isset($_SESSION['cart'])){
+            $_SESSION['cart'] = [];
+        }
+
+        $item = [
+            'nama' => $barang['nama_barang'],
+            'harga' => $barang['harga'],
+            'qty' => $qty,
+            'subtotal' => $barang['harga'] * $qty
+        ];
+
+        $_SESSION['cart'][] = $item;
+    } else {
+        echo "Barang tidak ditemukan!";
+    }
+}
+?>
+
+<h2>Transaksi</h2>
+
+<form method="POST">
+    Kode Barang:
+    <input type="text" name="kode_barang">
+
+    Jumlah:
+    <input type="number" name="qty">
+
+    <button type="submit" name="tambah">Tambah</button>
+</form>
+
+<hr>
+
+<h3>Keranjang</h3>
+
+<table border="1">
+<tr>
+    <th>Nama</th>
+    <th>Qty</th>
+    <th>Harga</th>
+    <th>Subtotal</th>
+</tr>
+
+<?php
+$total = 0;
+
+if(isset($_SESSION['cart'])){
+    foreach($_SESSION['cart'] as $item){
+        echo "<tr>
+            <td>{$item['nama']}</td>
+            <td>{$item['qty']}</td>
+            <td>{$item['harga']}</td>
+            <td>{$item['subtotal']}</td>
+        </tr>";
+
+        $total += $item['subtotal'];
+    }
+}
+?>
+
+</table>
+
+<h3>Total: Rp <?php echo $total; ?></h3>
